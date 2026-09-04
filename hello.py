f202a0770b14f358be1581361ab313dcfe0c2c2e
Javier Lee <2603064@sit.singaporetech.edu.sg>
2026-09-04 15:18:34 +0800

    Fun Conditions

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ print("My first post!")
 print("=======================")
 
 username = input("Enter your username: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
@@ -12,3 +12,6 @@ print("=======================")
 print("Username: ", username)
 print("Age: ", age)
 print("Category: ", category)
+
+if age>40 and category == 'fun':
+    print("You are old what is fun for you?")
