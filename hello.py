785dab2ab42eff0f9de7a431a205245af920e3a2
Javier Lee <2603064@sit.singaporetech.edu.sg>
2026-09-04 15:50:35 +0800

    Final Changes

--- hello.py
+++ hello.py
@@ -3,6 +3,23 @@ print("Welcome here")
 print("My first post!")
 print("=======================")
 
+username ='cool_creator'
+bio = "Fun Blogger"
+followers = 100
+
+followers += 50
+print("Day 1: ", followers)
+
+followers += 100
+print("Day 2: ", followers)
+
+followers -= 10
+print("Day 3: ", followers)
+
+print("Username: ", username)
+print("Bio: ", bio)
+print("Followers: ", followers)
+
 username = input("Enter your username: ")
 age = int(input("Enter your age: "))
 category = input("Enter Content Category: ")
