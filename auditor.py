a90142ebd861a75f90f0a7afd88a9937034cfb9a
Javier Lee <2603064@sit.singaporetech.edu.sg>
2026-09-11 16:14:04 +0800

    Changed lines

--- auditor.py
+++ auditor.py
@@ -5,11 +5,11 @@ while inventory < 500:
     stock_quantity = input("Enter stock quantity: ")
     if stock_quantity == 'quit':
         break
-    quantity = int(stock_quantity)
     if not stock_quantity.isdigit():
             print("Invalid input. Please enter a valid number.")
             failed_entries += 1
             continue
+    quantity = int(stock_quantity)
     if quantity < 0:
             print("Invalid input. Quantity cannot be negative.")
             failed_entries += 1
@@ -18,5 +18,7 @@ while inventory < 500:
     if inventory > 500:
             print("Inventory limit exceeded. Current inventory: ", inventory)
             break
+print ("Total units processed: ", inventory)
+print ("Number of Failed/Rejected Entries: ", failed_entries)
 
 
