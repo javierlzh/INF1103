ebd0640f584b74ddab0d25fa0d6837e1fef2aea3
Javier Lee <2603064@sit.singaporetech.edu.sg>
2026-09-11 14:41:40 +0800

    No string inputs allowed

--- auditor.py
+++ auditor.py
@@ -6,3 +6,8 @@ while inventory < 500:
     if stock_quantity == 'quit':
         break
     quantity = int(stock_quantity)
+    if not stock_quantity.isdigit():
+            print("Invalid input. Please enter a valid number.")
+            failed_entries += 1
+            continue
+
