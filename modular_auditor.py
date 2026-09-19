8bede0167165bc84c4a7f7868c1011c611202677
Javier Lee <2603064@sit.singaporetech.edu.sg>
2026-09-19 17:37:31 +0800

    Created Main Function and Call

--- modular_auditor.py
+++ modular_auditor.py
@@ -27,3 +27,27 @@ def generate_report(total_units, failed_attempts):
         print("-------------------")
         print("Total units processed: ", total_units)
         print("Number of Failed/Rejected Entries: ", failed_attempts)
+
+def main():
+        inventory = 0;
+        total_units = 0;
+        failed_entries = 0;
+
+        while True:
+              stock_quantity, invalid_count = get_valid_input()
+              failed_entries += invalid_count
+
+              if stock_quantity == 'quit':
+                break
+              else:
+                    inventory = process_delivery(inventory, stock_quantity)
+                    tax = calculate_tax(stock_quantity)
+                    total_units += 1
+                    print(f"Delivery of {stock_quantity} accepted. Tax: {tax}. New inventory total: {inventory}")
+
+        generate_report(total_units, failed_entries)
+        print("Final inventory count: ", inventory)
+        print("--------------------")
+
+if __name__ == "__main__":
+        main()
